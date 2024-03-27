721eb67cea7e052bd374e0f24c51f5b9062af2f5
Anthony Leung <pandaofc@gmail.com>
2024-03-27 16:26:02 +0000

    chore: update config

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
                     loader: 'babel-loader',
                 },
             },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
         ],
     },
 }
